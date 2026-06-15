c3f63a478db45bcb9ca41a10e39a7d3e01924ed7
neelabhishek20 <nneelabhishek20@gmail.com>
2026-06-15 15:08:44 +0530

    Create 387_First_Unique_Character_In_A_String.cpp
    LEETCODE 387

---
+++ Strings/387_First_Unique_Character_In_A_String.cpp
@@ -0,0 +1,20 @@
+// LeetCode 387: First Unique Character in a String
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int> freq(26, 0);
+
+        for(char ch : s) {
+            freq[ch - 'a']++;
+        }
+
+        for(int i = 0; i < s.length(); i++) {
+            if(freq[s[i] - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
