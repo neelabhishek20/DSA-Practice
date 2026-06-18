8bc4567630dc8680ea3faff97652791341edb42f
neelabhishek20 <nneelabhishek20@gmail.com>
2026-06-18 19:20:36 +0530

    Create 232_Implement_Queue_Using_Stacks.cpp
    LEET CODE 232

---
+++ Queues/232_Implement_Queue_Using_Stacks.cpp
@@ -0,0 +1,44 @@
+#include <stack>
+using namespace std;
+
+class MyQueue {
+private:
+    stack<int> s1;
+    stack<int> s2;
+
+public:
+    MyQueue() {
+
+    }
+
+    void push(int x) {
+        s1.push(x);
+    }
+
+    int pop() {
+
+        peek();
+
+        int front = s2.top();
+        s2.pop();
+
+        return front;
+    }
+
+    int peek() {
+
+        if(s2.empty()) {
+
+            while(!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+
+        return s2.top();
+    }
+
+    bool empty() {
+        return s1.empty() && s2.empty();
+    }
+};
