10d7586f6c1bfe64f7cf23df60d2b34ccf8dc7ea
neelabhishek20 <nneelabhishek20@gmail.com>
2026-06-22 11:27:50 +0530

    Create 862_Shortest_Subarray_With_Sum_At_Least_K.cpp
    LEETCODE 862

---
+++ Queues/862_Shortest_Subarray_With_Sum_At_Least_K.cpp
@@ -0,0 +1,37 @@
+// LeetCode 862: Shortest Subarray with Sum at Least K
+
+class Solution {
+public:
+    int shortestSubarray(vector<int>& nums, int k) {
+
+        int n = nums.size();
+        vector<long long> prefix(n + 1, 0);
+
+        for(int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+
+        deque<int> dq;
+        int ans = n + 1;
+
+        for(int i = 0; i <= n; i++) {
+
+            while(!dq.empty() &&
+                  prefix[i] - prefix[dq.front()] >= k) {
+
+                ans = min(ans, i - dq.front());
+                dq.pop_front();
+            }
+
+            while(!dq.empty() &&
+                  prefix[i] <= prefix[dq.back()]) {
+
+                dq.pop_back();
+            }
+
+            dq.push_back(i);
+        }
+
+        return ans == n + 1 ? -1 : ans;
+    }
+};
