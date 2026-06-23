aee331ffc83dcb5df5239858416472bdd01d9340
neelabhishek20 <nneelabhishek20@gmail.com>
2026-06-23 12:01:23 +0530

    Create 1696_Jump_Game_VI.cpp
    LEETCODE 1696

---
+++ Queues/1696_Jump_Game_VI.cpp
@@ -0,0 +1,39 @@
+// LeetCode 1696: Jump Game VI
+
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+
+        int n = nums.size();
+
+        vector<int> dp(n);
+        dp[0] = nums[0];
+
+        deque<int> dq;
+        dq.push_back(0);
+
+        for(int i = 1; i < n; i++) {
+
+            while(!dq.empty() && dq.front() < i - k) {
+                dq.pop_front();
+            }
+
+            dp[i] = nums[i] + dp[dq.front()];
+
+            while(!dq.empty() && dp[dq.back()] <= dp[i]) {
+                dq.pop_back();
+            }
+
+            dq.push_back(i);
+        }
+
+        return dp[n - 1];
+    }
+};
+
+INPUT:nums = {1,-1,-2,4,-7,3};
+k = 2;
+Possible Path: 1 → -1 → 4 → 3
+  Score: 1 + (-1) + 4 + 3 = 7
+  Output: 7
+  
